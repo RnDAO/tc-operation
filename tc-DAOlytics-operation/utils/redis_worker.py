8ddccfabaa353c9390420441bf6c90375cf49657
Mohammad Amin Dadgar <dadgaramin96@gmail.com>
2023-06-01 12:42:52 +0330

    Updated redis worker command! Redis scheduler is now supporting password!

---
+++ tc-DAOlytics-operation/utils/redis_worker.py
@@ -0,0 +1,18 @@
+import redis
+from rq import Queue, Worker, Connection
+from daolytics_uitls import get_redis_credentials
+
+if __name__ == "__main__":
+    redis_creds = get_redis_credentials()
+    host = redis_creds["host"]
+    port = redis_creds["port"]
+    password = redis_creds["pass"]
+    
+    r = redis.Redis(
+        host=host, 
+        port=port, 
+        password=password
+    )
+    worker = Worker(queues=['default'], connection=r)
+    worker.work(with_scheduler=True)
+
