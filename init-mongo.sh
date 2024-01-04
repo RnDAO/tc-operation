4f451f0e04cf7faf9fa137815c1bf813574aebc7
Cyrille <1618640+cyri113@users.noreply.github.com>
2024-01-04 11:33:03 +0100

    Update init-mongo.sh

--- init-mongo.sh
+++ init-mongo.sh
@@ -9,4 +9,20 @@ mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
     db.createUser({user: rootUser, pwd: rootPassword, roles: ["readWrite"]});
+
+    # REPLICA SET
+    var config = {
+        "_id": "dbrs",
+        "version": 1,
+        "members": [
+            {
+                "_id": 1,
+                "host": "mongodb-dev:27017",
+                "priority": 1
+            },
+        ]
+    };
+
+    rs.initiate(config, { force: true })
+    rs.status()
 EOF
